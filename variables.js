824fc99c30858b1a88235484e77f73f0090fc43c
Spencer Haka <spencer.haka@gmail.com>
2022-07-09 11:44:42 -0600

    add variable examples'

---
+++ variables.js
@@ -0,0 +1,11 @@
+var myName = "Spencer";
+var quote = "Of all the gin joints in all the towns in all the world, she walks into mine.";
+var pithySaying = "Live, Laugh, Love";
+
+var numberOfStates = 50;
+var numberOfBackstreetBoys = 5;
+var populationOfMalta = 525285;
+
+var doorIsOpen = true;
+var usernameValid = true;
+var isSignedIn = false;
